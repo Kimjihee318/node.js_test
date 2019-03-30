ad1a0baf3c5ae1aa392de3d2e201e2bcf78f37c8
jiheekim <jiheeKim318@gmail.com>
2019-03-30 21:40:26 +0900

    Advanced upload file_ used multer

--- project/app_upload.js
+++ project/app_upload.js
@@ -1,13 +1,37 @@
 let express = require('express');
 let multer = require('multer');
 
-// 업로드를 할 수있는 것을 받아주는 middle ware
-// dest : destination upload하는 파일이 들어가는 디렉토리 지정
-let upload = multer({ dest: 'uploads/' })
+/*
+ 업로드를 할 수있는 것을 받아주는 middle ware
+ diskStorage는 객체 리터럴 ({ blah})
+ cb는 콜백함수 
+*/
+let _storage = multer.diskStorage({
+  destination(req, file, cb) {
+    // 디렉토리 지정
+    cb(null, 'uploads/')
+    /*
+      ex) if(파일이 이미지이면) cb(null, 'upload/images)
+          else(파일이 이미지이면) cb(null, 'upload/text)
+    */
+    
+  },
+  filename(req, file, cb) {
+    // 파일 이름 지정
+    cb(null, file.originalname)
+    /*
+      ex) if(동일한 파일이 계속 올라간다면) cb(null, file.originalname' + n);
+    */
+
+  }
+});
+
+var upload = multer({ storage: _storage });
 
 let app = express();
 app.set('views', './views_file');
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+app.use('/user', express.static('uploads'));
 
 app.locals.pretty = true;
 // step1. 사용자가 파일을 업로드 하기 위한 form을 먼저 만드시오.
